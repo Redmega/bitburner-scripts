e32e5a2a59baa473346a0d88f42672ba777e586f
Angel Piscola <angelpiscola@gmail.com>
2021-12-19 23:42:59 -0500

    deploy: update scripts

--- dist/init.js
+++ dist/init.js
@@ -4,7 +4,7 @@ export async function main(ns) {
     ns.tprintf("INFO Downloading util script.");
     await ns.wget("https://raw.githubusercontent.com/Redmega/bitburner-scripts/main/dist/scripts/util.js", "/scripts/util.js");
     ns.tprintf("INFO Downloading sync script.");
-    await ns.wget("https://raw.githubusercontent.com/Redmega/bitburner-scripts/main/dist/sync.js", "/sync.js");
+    await ns.wget("https://raw.githubusercontent.com/Redmega/bitburner-scripts/main/dist/sync.js", "sync.js");
     ns.tprintf("INFO Spawning sync script");
     const pid = await ns.exec("sync.js", ns.getHostname(), 1);
     if (pid > 0) {
