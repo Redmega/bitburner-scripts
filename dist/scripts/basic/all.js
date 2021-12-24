e221935e71e1bf57298c1e4f5139382c865f36a2
Angel Piscola <angelpiscola@gmail.com>
2021-12-23 20:06:02 -0500

    feat: make script to run hack_loop on all hackable servers

---
+++ dist/scripts/basic/all.js
@@ -0,0 +1,8 @@
+import { getServers } from "../util";
+/** @param {NS} ns*/
+export async function main(ns) {
+    const servers = getServers(ns).filter((s) => s.root);
+    for (const server of servers) {
+        ns.run("/scripts/basic/hack_loop.js", 1, server.name);
+    }
+}
---
+++ src/scripts/basic/all.ts
@@ -0,0 +1,9 @@
+import { NS } from "types/NetscriptDefinitions";
+import { getServers } from "../util";
+
+export async function main(ns: NS) {
+  const servers = getServers(ns).filter((s) => s.root);
+  for (const server of servers) {
+    ns.run("/scripts/basic/hack_loop.js", 1, server.name);
+  }
+}
